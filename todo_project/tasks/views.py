436b7bfc6043daa83af37e320c77e0271223f72b
Veeveesh <vivianihesiaba.ad@gmail.com>
2026-06-30 13:08:09 +0100

    Found the bug and fixed it , final commit

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(requests):
+def home(request):
     return HttpResponse("<h1>This is our Texter Homepage <h1>")
 
-def about(requests):
+def about(request):
     return HttpResponse(f"<h1> You are currently in our about page<h1/> \n Texter is our first website used to find errors in your code")
 
-def contact(requests):
+def contact(request):
     return HttpResponse(f" Get in touch with us via github - Veeveesh")
